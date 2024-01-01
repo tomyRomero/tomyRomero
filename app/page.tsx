251fa970116958381bcbb2efe0c7634f771d8f33
Tomy Romero <tomyfletcher99@hotmail.com>
2024-01-01 16:06:39 -0400

    home page bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 
-import About from '@/components/About';
-import Hero from '@/components/Hero';
+import About from '../components/About';
+import Hero from '../components/Hero';
 
-import Skills from '@/components/Skills';
 export default function Home() {
 
   return (
