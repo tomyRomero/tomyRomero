d1f65c963225179d6ebbccc9e68ee180e8f5de86
Tomy Romero <tomyfletcher99@hotmail.com>
2024-01-11 19:08:13 -0400

    background fix

--- components/Tech.tsx
+++ components/Tech.tsx
@@ -13,7 +13,7 @@ const Tech = () => {
     triggerOnce: true, // Only trigger once when the component comes into view
   });
   return (
-    <motion.div className={`flex flex-row flex-wrap justify-center gap-10 px-4 py-8 ${theme === 'light' ? '' : 'bg-near-black'}`} ref={ref}
+    <motion.div className={`flex flex-row flex-wrap justify-center gap-10 px-4 py-8`} ref={ref}
     initial='hidden'
     animate={inView ? 'show' : 'hidden'}
     variants={slideIn("left", "tween", 0.2, 1)}
