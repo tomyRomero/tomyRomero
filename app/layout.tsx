57c388c57b92c74672a322e4069dadfa7cb87443
Tomy Romero <tomyfletcher99@hotmail.com>
2024-09-09 01:11:47 -0400

    layout bug fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Lora } from 'next/font/google'
 import './globals.css'
-import Navbar from '../components/nav/Navbar'
 import { AppProvider } from '@/lib/AppContext'
 import { Toaster } from "@/components/ui/toaster";
 import NewNav from '@/components/nav/newNav'
@@ -28,7 +27,6 @@ export default function RootLayout({
       <body className={`${interTight.className}`} >
         <AppProvider>
           <NewNav />
-          {/* <Navbar /> */}
           <div className='mt-14 max-sm:mt-2' >
           {children}
           </div>
