5c36f3b3d320f6084fd29e137adcda4e70965199
Tomy Romero <tomyfletcher99@hotmail.com>
2024-09-09 15:49:38 -0400

    fixed responsive for experience section

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,33 +1,58 @@
-"use client"
-import { experiences } from "@/constants";
-import { useAppContext } from "@/lib/AppContext";
+'use client';
+import { experiences } from '@/constants';
+import { useAppContext } from '@/lib/AppContext';
 
 export default function Experience() {
-
-    const {theme} = useAppContext();
+  const { theme } = useAppContext();
 
   return (
-    <section className={`w-full mt-10 mx-auto px-20 py-6 relative z-0 ${theme === "light" ? '' : 'bg-near-black'}`}
-     id="experience"
+    <section
+      className={`w-full mt-10 mx-auto px-6 md:px-20 py-6 relative z-0 ${
+        theme === 'light' ? '' : 'bg-near-black'
+      }`}
+      id="experience"
     >
-   
-        <div className="space-y-6">
-          <h2 className={`sectionHeadText ${theme === "light" ? 'text-primary-light' : 'text-primary-dark'}`}>Experience</h2>
-          <div className={`grid gap-8 ${theme === "light" ? 'text-near-black' : 'text-white'}`}>
-            {experiences.map((experience, index) => (
-              <div key={index} className="grid gap-2">
-                <div className="flex items-center justify-between">
-                  <div className="space-y-1">
-                    <h3 className={`text-heading2-bold max-sm:text-heading3-bold}`}>{experience.title}</h3>
-                    <p className={`text-heading3-bold max-sm:text-heading4-bold ${theme === "light" ? 'text-primary-light' : 'text-primary-dark'}`}>{experience.company}</p>
-                  </div>
-                  <p className="text-heading4-bold text-muted-foreground">{experience.date}</p>
+      <div className="space-y-6">
+        <h2
+          className={`text-heading2-bold md:text-heading1-bold ${
+            theme === 'light' ? 'text-primary-light' : 'text-primary-dark'
+          }`}
+        >
+          Experience
+        </h2>
+        <div
+          className={`grid gap-6 md:gap-8 ${
+            theme === 'light' ? 'text-near-black' : 'text-white'
+          }`}
+        >
+          {experiences.map((experience, index) => (
+            <div key={index} className="grid gap-2">
+              {/* Flex Direction adjusted for smaller screens */}
+              <div className="flex flex-col md:flex-row items-start md:items-center justify-between">
+                <div className="space-y-1">
+                  <h3 className="text-heading2-bold max-sm:text-heading3-bold">
+                    {experience.title}
+                  </h3>
+                  <p
+                    className={`text-heading3-bold max-sm:text-heading4-bold ${
+                      theme === 'light'
+                        ? 'text-primary-light'
+                        : 'text-primary-dark'
+                    }`}
+                  >
+                    {experience.company}
+                  </p>
                 </div>
-                <p className="">{experience.description}</p>
+                {/* Date positioned below title on smaller screens */}
+                <p className="text-heading4-bold text-muted-foreground sm:mt-0 mt-2">
+                  {experience.date}
+                </p>
               </div>
-            ))}
-          </div>
+              <p>{experience.description}</p>
+            </div>
+          ))}
         </div>
+      </div>
     </section>
   );
 }
